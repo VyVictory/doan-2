1fce560750ab163d2262b0919f63d1e125b46e62
lephuc0167283 <68628569+lephuc0167283@users.noreply.github.com>
2024-06-07 15:53:05 +0700

    Create getAllProfileAmin.jsx

---
+++ frontend/src/module/getAllProfileAmin.jsx
@@ -0,0 +1,28 @@
+//axios.get('http://localhost:5000/api/products/shop', { withCredentials: true })
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const GetAllProfileAmin = () => {
+    const [message, setMessage] = useState('');
+    const [listUser, setListUser] = useState([]);
+
+    useEffect(() => {
+        const fetchListUser = async () => {
+            try {
+                const response = await axios.get(`http://localhost:5000/api/users/`, { withCredentials: true });
+                setListUser(response.data);
+            } catch (error) {
+                if (error.response) {   
+                    setMessage('Error: ' + error.response.data);
+                } else {
+                    setMessage('An error occurred. Please try again later.');
+                }
+            }
+        };
+        fetchListUser();
+    }, []); // Empty dependency array to run effect only once
+
+    return { message, listUser };
+};
+
+export default GetAllProfileAmin;
