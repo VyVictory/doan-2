3d78d465651e1e751b35f67368aac0170b06f680
Nemo <76892046+NemoJohnFynz@users.noreply.github.com>
2024-06-10 14:14:18 +0700

    .

--- backend/backend/passport.js
+++ backend/backend/passport.js
@@ -1,14 +1,16 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID: "1082857622919-qki8gd1e85hnb5n3eoa6ojglsc71741f.apps.googleusercontent.com",
+    clientSecret: "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",
     callbackURL: "api/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    console.log(profile);
       return cb(err, user);
     // });
   }
